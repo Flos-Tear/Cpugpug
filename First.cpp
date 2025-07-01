32ad394af07417cc28a507b1c4dc233df2577855
Flos-Tear <j2.vvveded@gmail.com>
2025-07-01 11:57:43 +0700

    Create First.cpp

---
+++ First.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string s = "               "//อย่าลืมแทน s และเว้นวรรค
+  cout << endl << endl << "\n\n\n\n";
+  
+  cout << s <<"   ***  ***    " << endl;
+  cout << s <<"  *   **   *   " <<endl;
+  cout << s <<" *          *    " <<endl;
+  cout << s <<"  *        *     " <<endl;
+  cout << s <<"    *     *      " <<endl;
+  cout << s <<"      * *      " ;
+  
+  return 0;
+}
